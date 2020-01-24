987b8071e6d509c5245e0cb336e01b4a354261f7
yaziid <cantinanv@hotmail.com>
2020-01-24 23:29:33 +0100

    Update Yazid.java

--- Yazid.java
+++ Yazid.java
@@ -16,31 +16,22 @@ import java.lang.Object;
 import scala.Tuple2;
 
 public class Yazid {
+	
+	public static final String NAME = "Yazid"; //mettre lady gaga
+	
 	 public static void main(String[] args) {
 		 
 		 JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("JavaRandomForestClassificationExample")
 		            .setMaster("local[2]").set("spark.executor.memory","2g"));
 		 
 		 SparkSession spark = SparkSession
-
 				    .builder()
-
 				    .appName("Java Spark SQL Example")
-
 				    .getOrCreate();
+
 		 
-		 StructType schema = new StructType()
-
-				    .add("transactionId", "integer")
-
-				    .add("resourceId", "integer")
-
-				    .add("releaseId", "integer")
-
-				    .add("quantity", "integer");
-		 
-		 Dataset<Row> transaction = spark.read().format("csv").option("delimiter", ";").option("header", "true").load("file:///C://WinUtils//transaction.csv");
-		 Dataset<Row> resource = spark.read().format("csv").option("delimiter", ";").option("header", "true").load("file:///C://WinUtils//resource.csv");
+		 Dataset<Row> transaction = spark.read().format("csv").option("delimiter", ";").option("header", "true").load("file:///C://WinUtils//transaction.csv"); // TODO mettre un pipe dans delimiter
+		 Dataset<Row> resource = spark.read().format("csv").option("delimiter", ";").option("header", "true").load("file:///C://WinUtils//resource.csv"); //TODO mettre un pipe dans delimiter
 		 
 		 //Question 1
 		 Dataset<Row> dfResult1 = transaction.select(count((new Column("resourceId"))));
@@ -52,25 +43,29 @@ public class Yazid {
 		 //Question 3
 		 Dataset<Row> dfResult3 = transaction.select("*").orderBy(new Column("quantity").desc()).limit(10);
 		 
-		 //Question 4
-		 Dataset<Row> transaction_resource = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").where(new Column("artist").equalTo("Yazid")).select(sum(new Column("quantity")).multiply(0.15));
+		 //Question 4 T1
+		 Dataset<Row> transaction_resource = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").where(new Column("artist").equalTo(NAME)).select(sum(new Column("quantity")).multiply(0.15));
 		 
-		 		 //Question 5 T2
-		 Dataset<Row> transaction_resource1 = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").where(new Column("artist").equalTo("Yazid"))
+		 //Question 5 T2
+		 Dataset<Row> transaction_resource1 = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").where(new Column("artist").equalTo(NAME))
 				 .select(sum(when(new Column("quantity").$less(500), new Column("quantity").multiply(0.10)
 								 ).when(new Column("quantity").$greater$eq(500), new Column("quantity").multiply(0.20))));
 		 
-		 		 //Question 6 T3
+		 //Question 6 T3
 		 Dataset<Row> transaction_resource2 = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").
-				 where(new Column("artist").equalTo("Yazid")).
+				 where(new Column("artist").equalTo(NAME)).
 				 select(sum(when(new Column("quantity").$less(500), new Column("quantity").multiply(0.1)).
 						 when(new Column("quantity").$greater$eq(500), new Column("quantity").$minus(500).multiply(0.2).$plus(500*0.1))));
 		 
-		 		 //Question 7 T4
+		 //Question 7 T4
 		 Dataset<Row> transaction_resource3 = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").
-				 select(lit(10000).divide(count(new Column("transactionId")))).where(new Column("artist").equalTo(NAME));
+				 select(lit(10000).divide(count(new Column("transactionId"))));
 		 
-		 transaction_resource.show();
+		 //Question 8 T5
+		 Dataset<Row> transaction_resource4 = transaction.join(resource, transaction.col("resourceId").equalTo(resource.col("resourceId")), "inner").where(new Column("artist").equalTo(NAME)).
+				 select(lit(10000).divide(sum(new Column("quantity"))).alias("total"), new Column("quantity").alias("somme")).where(new Column("artist").equalTo(NAME));
+		 
+		 transaction_resource4.show();
 
 	     
 		sc.close();
